692efa3d4eec9a2d41d3b98879fc6ccd494a36ac
soumaeda <soumaeda1126@icloud.com>
2023-08-29 08:47:54 +0900

    add production

---
+++ app/controllers/productions_controller.rb
@@ -0,0 +1,7 @@
+class ProductionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'productions/index'
+  get 'productions/show'
+  get 'production/index'
+  get 'production/show'
   get '/' => 'homes#top'
 end
